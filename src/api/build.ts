3ce4ab239b444322ab504cf5b3e77b813f9e33b7
Rob Burger <rob.burgs@gmail.com>
2020-05-06 00:42:44 +0200

    Add builds method and tests

--- src/api/build.ts
+++ src/api/build.ts
@@ -1,4 +1,4 @@
-import { API_JOB, BuildResponse } from "../types";
+import { API_JOB, BuildResponse, BuildsResponse } from "../types";
 import { client } from "../client";
 
 /**
@@ -17,4 +17,22 @@ export function getBuild(
 ): Promise<BuildResponse> {
   const url = `${API_JOB}/${buildPath}/api/json`;
   return client(username, token, jenkinsHost).get<BuildResponse>(url);
+}
+
+/**
+ * Get build details for a job
+ *
+ * @param username - The username of a Jenkins user
+ * @param token - The Jenkins API token associated with the user
+ * @param jenkinsHost - The hostname of the Jenkins instance
+ * @param jobName - The name of the job
+ */
+export function getBuilds(
+  username: string,
+  token: string,
+  jenkinsHost: string,
+  jobName: string
+): Promise<BuildsResponse> {
+  const url = `${API_JOB}/${jobName}/api/json?depth=1`;
+  return client(username, token, jenkinsHost).get<BuildsResponse>(url);
 }
